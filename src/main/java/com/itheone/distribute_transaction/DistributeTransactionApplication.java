56a2c5fdc753655c9c8f6b69e81f8c287bae8604
itheone <caohaifengx@163.com>
2019-12-14 12:44:02 +0800

    first commit

---
+++ src/main/java/com/itheone/distribute_transaction/DistributeTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.itheone.distribute_transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributeTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributeTransactionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itheone/distribute_transaction/DistributeTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheone.distribute_transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributeTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
